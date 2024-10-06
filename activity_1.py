eb90244a656da54b13e7a0fe0f97a060f3bfd3f2
Moeez Omair <104922728+mozzez-943@users.noreply.github.com>
2024-10-06 12:58:13 -0400

    Update activity_1.py

--- activity_1.py
+++ activity_1.py
@@ -1,6 +1,8 @@
 source_file = open('a1_dishes_by_votes.txt', 'r')
 destination_file = open('a1_filtered_dishes.txt', 'w')
+
 line = source_file.readline()
+
 while line:
     dish, score = line.split(':')
     if int(score) >= 10:
