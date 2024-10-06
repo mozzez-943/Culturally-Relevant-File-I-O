86b11b497b0724953c01e4eb153a71c08f73ed86
Moeez Omair <104922728+mozzez-943@users.noreply.github.com>
2024-10-06 15:16:30 -0400

    updated activity 2

--- activity_2.py
+++ activity_2.py
@@ -1,14 +1,13 @@
 import csv
 
-source_file = open('a2_students.csv', 'r')
-destination_file = open('a2_filtered_students.txt', 'w')
+source_file = open('a2_cultural_foods.csv', 'r')
+destination_file = open('a2_foods_veg_halal.txt', 'w')
 
 reader = csv.DictReader(source_file)
 
 for row in reader:
-    # Each row is a dictionary
-    if float(row['Grade']) >= 50:
-        # Each row is a dictionary
+    # Filtering for vegetarian and halal, treating each row as a dictionary
+    if (row['Vegetarian'] == "yes") and (row['Halal'] == "yes"):
         destination_file.write(f'{row["Name"]}\n')
 
 source_file.close()
